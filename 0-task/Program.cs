63d37fb56da741844260b3e1f810c62f41326dfd
raiL0ng <19nik.nikitov.97@mail.ru>
2023-04-18 23:20:25 +0400

    Edit smth

--- 0-task/Program.cs
+++ 0-task/Program.cs
@@ -2,30 +2,26 @@
 using System;
 using System.IO;
 
-namespace MyApp // Note: actual namespace depends on the project name.
-{
-    
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
             string? line, s;
             int n, pos;
-            try
-            {
+            SortedDictionary<string, int> dict = new SortedDictionary<string, int>();
+            try {
                 StreamReader sr = new StreamReader("test.txt");
                 line = sr.ReadLine();
-                Dictionary<string, int> dict = new Dictionary<string, int>();
-                while (!String.IsNullOrEmpty(line))
-                {
+                while (!String.IsNullOrEmpty(line)) {
                     n = line.Length;
                     pos = 0;
                     for (int i = 0; i < n; i++) {
-                        if (!Char.IsLetter(line[i]))
-                        {
-                            s = line.Substring(pos, i - pos);
-                            if (!dict.ContainsKey(s))
-                            {
+                        if (!Char.IsLetter(line[i])) {
+                            if (i - pos <= 0) {
+                                pos++;
+                                continue;
+                            }
+                            s = line.Substring(pos, i - pos).ToLower();
+                            if (!dict.ContainsKey(s)) {
                                 dict.Add(s, 0);
                             }   
                             dict[s] += 1;
@@ -36,23 +32,37 @@ namespace MyApp // Note: actual namespace depends on the project name.
                             // }
                         }
                     }
-                    Console.WriteLine(line);
+                    // Console.WriteLine(line);
                     line = sr.ReadLine();
                 }
                 sr.Close();
-                foreach (var el in dict)
-                {
-                    Console.WriteLine("Key: \"{0}\", Value: {1}", el.Key, el.Value);
-                }
+                // foreach (var el in dict) {
+                //     Console.WriteLine("{0}\t{1}", el.Key, el.Value);
+                // }
+
             }
-            catch(Exception e)
-            {
+            catch(Exception e) {
                 Console.WriteLine("Exception: " + e.Message);
             }
-            finally
-            {
-                Console.WriteLine("\nExecuting finally block.");
+            finally {
+                Console.WriteLine("\nВсе слова были успешно посчитаны\n");
             }
+            var sortedDict = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            try {
+                
+                StreamWriter sw = new StreamWriter("words-freq.txt");
+                foreach (var el in sortedDict) {
+                    sw.WriteLine("{0}\t\t{1}", el.Key, el.Value);
+                }
+                sw.Close();
+            }
+            catch(Exception e) {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally {
+                Console.WriteLine("Все слова записаны в файл \"words-freq.txt\" в порядке убывания");
+            }
+
         }
     }
 }
