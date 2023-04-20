0a7ae3980dc569ea740b59a6d50821973ac3aff7
raiL0ng <19nik.nikitov.97@mail.ru>
2023-04-20 11:48:15 +0400

    Final changes?

--- 0-task/Program.cs
+++ 0-task/Program.cs
@@ -1,13 +1,12 @@
-﻿
-using System;
+﻿using System;
 using System.IO;
 
 namespace MyApp {
     internal class Program {
         static void Main(string[] args) {
             string? line, s;
-            int n, pos;
-            SortedDictionary<string, int> dict = new SortedDictionary<string, int>();
+            int n, pos, maxlen = 0;
+            Dictionary<string, int> dict = new Dictionary<string, int>();
             try {
                 StreamReader sr = new StreamReader("test.txt");
                 line = sr.ReadLine();
@@ -21,25 +20,17 @@ namespace MyApp {
                                 continue;
                             }
                             s = line.Substring(pos, i - pos).ToLower();
+                            if (maxlen < s.Length) maxlen = s.Length;
                             if (!dict.ContainsKey(s)) {
                                 dict.Add(s, 0);
                             }   
                             dict[s] += 1;
                             pos = i + 1;
-                            // foreach (var el in dict)
-                            // {
-                            //     Console.WriteLine("Key: \"{0}\", Value: {1}", el.Key, el.Value);
-                            // }
                         }
                     }
-                    // Console.WriteLine(line);
                     line = sr.ReadLine();
                 }
                 sr.Close();
-                // foreach (var el in dict) {
-                //     Console.WriteLine("{0}\t{1}", el.Key, el.Value);
-                // }
-
             }
             catch(Exception e) {
                 Console.WriteLine("Exception: " + e.Message);
@@ -51,8 +42,18 @@ namespace MyApp {
             try {
                 
                 StreamWriter sw = new StreamWriter("words-freq.txt");
+                maxlen += 2;
+                string tabs;
+                int tabs_cnt, tmp_len;
                 foreach (var el in sortedDict) {
-                    sw.WriteLine("{0}\t\t{1}", el.Key, el.Value);
+                    tabs_cnt = 0;
+                    tabs = "";
+                    tmp_len = maxlen - el.Key.Length;
+                    while (tabs_cnt < tmp_len) {
+                        tabs_cnt++;
+                        tabs += ' ';
+                    }
+                    sw.WriteLine("{0}{1}{2}", el.Key, tabs, el.Value);
                 }
                 sw.Close();
             }
